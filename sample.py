f0d362b6b2e436d68bd9e0f294c777a40e08dbf6
sethrkaufman <kaufmans@amazon.com>
2016-01-11 18:54:22 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,19 +1,3 @@
-Skip to content
-This repository  
-Search
-Pull requests
-Issues
-Gist
- @sethrkaufman
- Watch 8
-  Star 3
-  Fork 4 accesio/AIOUSB
- Code  Issues 4  Pull requests 0  Wiki  Pulse  Graphs
-Branch: master Find file Copy pathAIOUSB/AIOUSB/samples/USB-DIO-32/sample.py
-dabc488  on Jun 9, 2014
-@accesio accesio ENH: Fixes for build on Cygwin for make and cmake
-1 contributor
-RawBlameHistory     Executable File  110 lines (92 sloc)  3.48 KB
 #!/usr/bin/python
 # @file   sample.py
 # @author $Format: %an <%ae>$
@@ -33,31 +17,31 @@ MAX_DIO_BYTES = 32
 # Simple class for keeping track of deviecs found
 #
 class Device:
-    outputMask = NewAIOChannelMaskFromStr("1111")
-    readBuffer = DIOBuf(MAX_DIO_BYTES )
-    writeBuffer = DIOBuf( MAX_DIO_BYTES )
-    name = ""
-    serialNumber = 0
-    index = 0
-    numDIOBytes = 0
-    numCounters = 0
-    productID = 0
-    def __init__(self, **kwds):
-        self.__dict__.update(kwds)
+ outputMask = NewAIOChannelMaskFromStr("1111")
+ readBuffer = DIOBuf(MAX_DIO_BYTES)
+ writeBuffer = DIOBuf(MAX_DIO_BYTES)
+ name = ""
+ serialNumber = 0
+ index = 0
+ numDIOBytes = 0
+ numCounters = 0
+ productID = 0
+ def __init__(self, **kwds):
+  self.__dict__.update(kwds)
 
 
 devices = []                    # Array of our Devices
 number_devices = 1
 
-print """USB-DIO-32 sample program version 1.17, 26 November 2009
-This program demonstrates communicating with %d USB-DIO-32 devices on + 
-AIOUSB library version %s, %s
-the same USB bus. For simplicity, it uses the first %d such devices 
+print """USB-DIO-32 sample program version 1.17, 26 November 2009 \
+This program demonstrates communicating with %d USB-DIO-32 devices on + \
+AIOUSB library version %s, %s \
+the same USB bus. For simplicity, it uses the first %d such devices \
 found on the bus""" % ( number_devices, AIOUSB_GetVersion(), AIOUSB_GetVersionDate() , number_devices )
-         
+
 result = AIOUSB_Init()
 if result != AIOUSB_SUCCESS:
-    sys.exit("Unable to initialize USB devices")
+ sys.exit("Unable to initialize USB devices")
 
 devicesFound = 0
 deviceMask = GetDevices()
@@ -66,30 +50,30 @@ index = 0
 AIOUSB_ListDevices()
 
 while deviceMask > 0 and len(devices) < number_devices :
-    if (deviceMask & 1 ) != 0:
-        obj = GetDeviceInfo( index )
-        if obj.PID == USB_DIO_32 or obj.PID == USB_DIO_16A :
-            devices.append( Device( index=index, productID=obj.PID, numDIOBytes=obj.DIOBytes,numCounters=obj.Counters ))
-    index += 1
-    deviceMask >>= 1
+ if (deviceMask & 1 ) != 0:
+  obj = GetDeviceInfo( index )
+  if obj.PID == USB_DIO_32 or obj.PID == USB_DIO_16A :
+   devices.append( Device( index=index, productID=obj.PID, numDIOBytes=obj.DIOBytes,numCounters=obj.Counters ))
+  index += 1
+  deviceMask >>= 1
 try:
-    device = devices[0]
+ device = devices[0]
 except IndexError:
-    print """No devices were found. Please make sure you have at least one 
-ACCES I/O Products USB device plugged into your computer"""
-    sys.exit(1)
+ print """No devices were found. Please make sure you have at least one \
+ ACCES I/O Products USB device plugged into your computer"""
+ sys.exit(1)
 
 AIOUSB_SetCommTimeout( device.index, 1000 )
 AIOChannelMaskSetMaskFromStr( device.outputMask, "1111" )
 print "Mask was %s " % ( AIOChannelMaskToString( device.outputMask ))
 
 for port in range(0x20):
-    print "Using value %d" % (port)
-    DIOBufSetIndex( device.writeBuffer, port, 1 );
-    print DIOBufToString( device.writeBuffer )
-    result = DIO_Configure( device.index, AIOUSB_FALSE, device.outputMask , device.writeBuffer )
-    #print "Result was %d" % (result)
-    time.sleep(1/10.0)
+ print "Using value %d" % (port)
+ DIOBufSetIndex( device.writeBuffer, port, 1 );
+ print DIOBufToString( device.writeBuffer )
+ result = DIO_Configure( device.index, AIOUSB_FALSE, device.outputMask , device.writeBuffer )
+ #print "Result was %d" % (result)
+ time.sleep(1/10.0)
 
 # ONLY Port D
 AIOChannelMaskSetMaskFromStr(device.outputMask, "1000" );
@@ -114,14 +98,10 @@ result = DIO_Configure( device.index, AIOUSB_FALSE, device.outputMask , device.w
 tmpbuf = list("00000000000000000000000000000000")
 
 for i in range(len(tmpbuf)):
-    if i >= 1:
-        tmpbuf[i] = "0"
+ if i >= 1:
+  tmpbuf[i] = "0"
 
-    tmpbuf[i] = '1'
-    DIOBufReplaceBinString(device.writeBuffer, "".join(tmpbuf) )
-    result = DIO_Configure( device.index, AIOUSB_FALSE, device.outputMask , device.writeBuffer )    
-    time.sleep(1/10.0)
-
-
-Status API Training Shop Blog About Pricing
-© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+ tmpbuf[i] = '1'
+ DIOBufReplaceBinString(device.writeBuffer, "".join(tmpbuf) )
+ result = DIO_Configure( device.index, AIOUSB_FALSE, device.outputMask , device.writeBuffer )    
+ time.sleep(1/10.0)
